7658c63deb07121ad701b03309728229ab30d188
Sven <sven@139.com>
2018-05-29 21:32:58 +0800

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -23,12 +23,6 @@ let config = {
           name: 'vendor', // 打包后的文件名，任意命名
           // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
           priority: 10
-        },
-        utils: {
-          // 抽离自己写的公共代码，utils这个名字可以随意起
-          chunks: 'initial',
-          name: 'utils', // 任意命名
-          minSize: 0 // 只要超出0字节就生成一个新包
         }
       }
     }
