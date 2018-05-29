a7dd3c6071640693308a9fd19a635a3111667ad2
Sven <sven@139.com>
2018-05-29 20:42:02 +0800

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -5,100 +5,106 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 let config = {
-  entry: './example/src/index',    // 入口文件
+  entry: './example/src/index', // 入口文件
   output: {
-    filename: 'bundle.[hash:4].js',    // 添加hash可以防止文件缓存，每次都会生成4位的hash串
-    path: path.resolve('dist')  // 打包后的目录，必须是绝对路径
+    filename: 'bundle.[hash:4].js', // 添加hash可以防止文件缓存，每次都会生成4位的hash串
+    path: path.resolve('dist') // 打包后的目录，必须是绝对路径
   },
   devtool: 'source-map',
   optimization: {
     splitChunks: {
       cacheGroups: {
-        vendor: {   // 抽离第三方插件
-          test: /node_modules/,   // 指定是node_modules下的第三方包
+        vendor: {
+          // 抽离第三方插件
+          test: /node_modules/, // 指定是node_modules下的第三方包
           chunks: 'initial',
-          name: 'vendor',  // 打包后的文件名，任意命名    
+          name: 'vendor', // 打包后的文件名，任意命名
           // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
-          priority: 10    
+          priority: 10
         },
-        utils: { // 抽离自己写的公共代码，utils这个名字可以随意起
+        utils: {
+          // 抽离自己写的公共代码，utils这个名字可以随意起
           chunks: 'initial',
-          name: 'utils',  // 任意命名
-          minSize: 0    // 只要超出0字节就生成一个新包
+          name: 'utils', // 任意命名
+          minSize: 0 // 只要超出0字节就生成一个新包
         }
       }
     }
   },
   resolve: {
-    extensions: ['.js', '.json', '.jsx', '.scss'],
+    extensions: ['.js', '.json', '.jsx', '.scss']
   },
   stats: {
     colors: true,
-    modules: false,
+    modules: false
   }
-}
+};
 
 config.module = {
   rules: [
-  {
-    test: /\.scss$/,
-    use: ExtractTextPlugin.extract({
-      // 将css用link的方式引入
-      fallback: 'style-loader',
-      use: ['css-loader', 'sass-loader']
-    })
-  },
-  {
-    test: /\.(jpe?g|png|gif)$/,
-    use: [{
-      loader: 'url-loader',
-      options: {
-        limit: 8192,    // 小于8k的图片自动转成base64格式，并且不会存在实体图片
-        outputPath: 'images/'   // 图片打包后存放的目录
-      }
-    }]
-  },
-  { test: /\.(js|jsx)$/,
-    include: /example/,
-    exclude: /node_modules/,
-    loader: 'babel-loader?cacheDirectory'
-  },
-  {
-    test: /\.(eot|ttf|woff|svg)$/,
-    use: 'file-loader'
-  },
-  {
-    test: /\.(htm|html)$/,
-    use: 'html-withimg-loader'
-  }]
-}
+    {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        // 将css用link的方式引入
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader']
+      })
+    },
+    {
+      test: /\.(jpe?g|png|gif)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192, // 小于8k的图片自动转成base64格式，并且不会存在实体图片
+            outputPath: 'images/' // 图片打包后存放的目录
+          }
+        }
+      ]
+    },
+    {
+      test: /\.(js|jsx)$/,
+      include: /example/,
+      exclude: /node_modules/,
+      loader: 'babel-loader?cacheDirectory'
+    },
+    {
+      test: /\.(eot|ttf|woff|svg)$/,
+      use: 'file-loader'
+    },
+    {
+      test: /\.(htm|html)$/,
+      use: 'html-withimg-loader'
+    }
+  ]
+};
 
 config.plugins = [
   // 通过new一下这个类来使用插件
   new HtmlWebpackPlugin({
-      // 用哪个html作为模板
-      // 在 dist 目录下创建一个index.html页面当做模板来用
-      template: './example/index.html',
-      hash: true, // 会在打包好的bundle.js 后面加上 hash串
+    // 用哪个html作为模板
+    // 在 dist 目录下创建一个index.html页面当做模板来用
+    template: './example/index.html',
+    hash: true // 会在打包好的bundle.js 后面加上 hash串
   }),
   // // 拆分后会把 css 文件放到 dist 目录下的css/style.css
   new ExtractTextPlugin('css/style.css'),
   // 打包前先清空
   new CleanWebpackPlugin('dist'),
   // 热更新
-  new webpack.HotModuleReplacementPlugin() 
-]
+  new webpack.HotModuleReplacementPlugin()
+];
 
 config.devServer = {
   contentBase: './example',
-  host: 'localhost',      // 默认是localhost
-  port: 3000,             // 端口
-  open: true,             // 自动打开浏览器
-  hot: true,               // 开启热更新
+  host: 'localhost', // 默认是localhost
+  port: 3000, // 端口
+  open: true, // 自动打开浏览器
+  hot: true, // 开启热更新
   stats: {
     colors: true,
-    modules: false,
+    modules: false
   }
-}
+};
 
 module.exports = config;
