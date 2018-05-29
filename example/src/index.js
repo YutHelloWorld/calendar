253876161bf0955bb551ca51d1e323285b8f812c
Sven <sven@139.com>
2018-05-29 20:37:23 +0800

    lint

--- example/src/index.js
+++ example/src/index.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import Calendar from '../../lib'
-import { render } from 'react-dom'
+import React from 'react';
+import Calendar from '../../lib';
+import { render } from 'react-dom';
 
-const MOUNT_NODE = document.getElementById('root')
+const MOUNT_NODE = document.getElementById('root');
 
 function App() {
   return (
-    <div style={{
-      position: 'absolute',
-      top: '0',
-      bottom: '0',
-      left: '0',
-      right: '0',
-      margin: 'auto',
-      width: '277px',
-      height: '355px'
-    }}
+    <div
+      style={{
+        position: 'absolute',
+        top: '0',
+        bottom: '0',
+        left: '0',
+        right: '0',
+        margin: 'auto',
+        width: '277px',
+        height: '355px'
+      }}
     >
-            <Calendar               />
+      <Calendar />
     </div>
-  )
+  );
 }
-render(<App />, MOUNT_NODE)
+render(<App />, MOUNT_NODE);
 if (module.hot) {
   // 实现热更新
   module.hot.accept();
