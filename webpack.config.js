7b9150998e8587b6e13f376204cfa49a300ecf7c
Sven <sven@139.com>
2018-05-30 11:29:53 +0800

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -12,19 +12,9 @@ let config = {
     path: path.resolve('dist'), // 打包后的目录，必须是绝对路径
     publicPath: __DEV__ ? '/' : '/calendar/'
   },
-  devtool: __DEV__ ? 'cheap-module-source-map' : 'source-map',
   optimization: {
     splitChunks: {
-      cacheGroups: {
-        vendor: {
-          // 抽离第三方插件
-          test: /node_modules/, // 指定是node_modules下的第三方包
-          chunks: 'initial',
-          name: 'vendor', // 打包后的文件名，任意命名
-          // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
-          priority: 10
-        }
-      }
+      chunks: 'all'
     }
   },
   resolve: {
